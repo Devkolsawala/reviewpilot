da049259febc1e62dd080612f5fe08b2c4248e25
Dev <97210570+Devkolsawala@users.noreply.github.com>
2026-04-28 13:58:48 +0530

    Update YouTube demo URL to full link

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,3 @@
 // TODO: Dev to replace REPLACE_WITH_VIDEO_ID with the actual YouTube video ID once uploaded
 export const YOUTUBE_DEMO_URL =
-  "youtu.be/WXVq7twjiVw";
+  "https://www.youtube.com/watch?v=WXVq7twjiVw";
